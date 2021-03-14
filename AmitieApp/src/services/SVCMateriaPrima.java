ad8439f8c208bac6c298feb950b132286a4a0d9f
kaio.barbosa <kaio.barbosa@conexos.inf.br>
2021-03-13 21:37:33 -0300

    Criação diagrama CDP.

--- AmitieApp/src/services/SVCMateriaPrima.java
+++ AmitieApp/src/services/SVCMateriaPrima.java
@@ -5,11 +5,49 @@
  */
 package services;
 
+import dao.MateriaPrimaDAO;
+import objetos.MateriaPrima;
+
 /**
  *
  * @author kaiof
  */
 public class SVCMateriaPrima {
+    public boolean insert_MateriaPrima(String des,Float vlr_custo, Float vlr_venda,Integer cod_unidade,String fabricante,String marca){
+        MateriaPrima materiaPrima = new MateriaPrima();
+        materiaPrima.setVlr_custo(vlr_custo);
+        materiaPrima.setVlr_venda(vlr_venda);
+        materiaPrima.setCod_unidade(cod_unidade);
+        materiaPrima.setFabricante(fabricante);
+        materiaPrima.setMarca(marca);
+        return MateriaPrimaDAO.insert(materiaPrima);
+    }
+    public boolean delete_MateriaPrima(Integer cod){
+        MateriaPrima materiaPrima = new MateriaPrima();
+        materiaPrima.setCod(cod);
+        return MateriaPrimaDAO.delete(materiaPrima);
+    }
+    public boolean update_MateriaPrima(Integer cod,String des,Float vlr_custo, Float vlr_venda,Integer cod_unidade,String fabricante,String marca){
+        MateriaPrima materiaPrima = new MateriaPrima();
+        materiaPrima.setCod(cod);
+        materiaPrima.setVlr_custo(vlr_custo);
+        materiaPrima.setVlr_venda(vlr_venda);
+        materiaPrima.setCod_unidade(cod_unidade);
+        materiaPrima.setFabricante(fabricante);
+        materiaPrima.setMarca(marca);
+        return MateriaPrimaDAO.update(materiaPrima);
+    }
+    
+    public Integer getSaldo(Integer cod){
+        MateriaPrima materiaPrima = new MateriaPrima();
+        materiaPrima.setCod(cod);
+        return MateriaPrimaDAO.getSaldo(materiaPrima);
+    }
+    public String getMateria(Integer cod){
+        MateriaPrima materiaPrima = new MateriaPrima();
+        materiaPrima.setCod(cod);
+        return MateriaPrimaDAO.getMateria(materiaPrima).getDes();
+    }
     
     
 }
